40f06b72420c617440c72a88d2601a93624b62e3
latifiann <latifian.iman44@gmail.com>
2024-12-26 15:47:51 +0700

    feat: add style and theme on view activity

--- app/src/main/java/com/dicoding/myintentapp/ViewActivity.kt
+++ app/src/main/java/com/dicoding/myintentapp/ViewActivity.kt
@@ -1,11 +1,24 @@
 package com.dicoding.myintentapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class ViewActivity : AppCompatActivity() {
+class ViewActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view)
+
+        val btnBuy: Button = findViewById(R.id.btn_buy)
+        btnBuy.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_buy) {
+            val backMain = Intent(this, MainActivity::class.java)
+            startActivity(backMain)
+        }
     }
 }
